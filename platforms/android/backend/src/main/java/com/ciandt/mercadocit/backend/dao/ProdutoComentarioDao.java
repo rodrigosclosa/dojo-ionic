b82a6efce9a7a9fb6435e507da524c6bdb65dde0
Thales Lima <thales@ciandt.com>
2016-04-07 12:57:48 -0300

    Produto comentario.

---
+++ platforms/android/backend/src/main/java/com/ciandt/mercadocit/backend/dao/ProdutoComentarioDao.java
@@ -0,0 +1,9 @@
+package com.ciandt.mercadocit.backend.dao;
+
+import com.ciandt.mercadocit.backend.entity.ProdutoComentario;
+
+/**
+ * Created by Garage on 07/04/16.
+ */
+public class ProdutoComentarioDao extends GenericDao<ProdutoComentario> {
+}

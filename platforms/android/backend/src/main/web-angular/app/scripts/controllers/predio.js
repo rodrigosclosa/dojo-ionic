70390e4ed398a9606000bfe6130a2fb6afa4b7e9
rodrigosclosa <rodrigo.sclosa@gmail.com>
2016-06-09 12:39:46 -0300

    predios

--- platforms/android/backend/src/main/web-angular/app/scripts/controllers/predio.js
+++ platforms/android/backend/src/main/web-angular/app/scripts/controllers/predio.js
@@ -11,23 +11,61 @@ angular.module('adminApp')
   .controller('PredioCtrl', function ($scope,api) {
       
       
-    $scope.list = [];
-    api.get('https://mercado-cit.appspot.com/_ah/api/predio/v1/predio')
+    $scope.predios = [];
+        // New item
+    $scope.predio = {
+        'nome' : '',
+        'icone' : 'teste',
+        'idBase' : ''
+    };
+
+    $scope.bases = [];
+
+    var loadPredios = function(){
+        api.get('predio/v1/predio')
         .success(function(response){
-            var items = response.items;
-            var size  = items.length;
-            console.log(response.items);
-            for(var i=0; i<size; i++){
-                $scope.list.push({name:items[i].nome});
-            }
+            console.log(response)
+            $scope.predios = response.items;
         })
         .error(function(err){
             console.log(err)
-        })
+        });
+    }
+    
+    //
+    $scope.editar = function(item) {
+        $scope.predio = item;
+    };
+    
+    $scope.excluir = function(item) {
+        var predio = $scope.predios.splice($scope.predios.indexOf(item), 1);
+        console.log(predio);
+        api.delete('predio/v1/predio/' + predio[0].id);
+    };
+
+    // Post data to api
+    $scope.submit = function () {
+       var newPredio = $scope.predio;
+       newPredio.idBase = $scope.base.id;
+       console.log(newPredio);
+       api.post('predio/v1/predio', newPredio).then(loadPredios);
+       
+    };
+
+    $scope.load = function() {
+       loadPredios();
         
-        $scope.submit = function () {
-            $scope.list.push($scope.predio);
-            $scope.predio = {};
-        };
+        api.get('base/v1/base')
+        .success(function(response){
+            console.log(response)
+            $scope.bases = response.items;
+        })
+        .error(function(err){
+            console.log(err)
+        });
+    }
+    
+    
+    $scope.load();
     
   });

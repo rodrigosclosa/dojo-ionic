9838fabb1ccea271f150abe83c111147b527a611
Thales Lima <thales@ciandt.com>
2016-06-16 12:26:46 -0300

    Acerto na versão do IOS

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -52,6 +52,13 @@ module.exports = [
 ];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "cordova-plugin-console": "1.0.2",
+    "cordova-plugin-device": "1.1.1",
+    "cordova-plugin-splashscreen": "3.1.0",
+    "cordova-plugin-statusbar": "2.1.1",
+    "cordova-plugin-whitelist": "1.2.1",
+    "ionic-plugin-keyboard": "1.0.8"
+}
 // BOTTOM OF METADATA
 });

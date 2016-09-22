c41e61a2283df5e553d4e8b92e22f33f63ef3a98
Rodrigo Sclosa <rodrigogs@ciandt.com>
2016-09-22 11:42:20 -0300

    Add header.js

---
+++ platforms/android/backend/src/main/web-angular/app/scripts/controllers/header.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adminApp.controller:HeaderCtrl
+ * @description
+ * # HeaderCtrl
+ * Controller of the adminApp
+ */
+angular.module('adminApp')
+  .controller('HeaderCtrl', function($scope, api, $location, $window) {
+
+    $scope.isActive = function (viewLocation) {
+       var active = (viewLocation === $location.path());
+       return active;
+    };
+  });
